596562d102b3bb7b9a6946bc521b3dbcf193f2b5
Ryan Wesley <ryanjwesley@gmail.com>
2020-11-10 11:58:02 -0800

    remove logo import

--- Hungry/src/components/app/App.js
+++ Hungry/src/components/app/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import logo from '../../logo.svg';
+
 import BusinessList from '../businessList/BusinessList';
 import SearchBar from '../searchBar/SearchBar';
 
