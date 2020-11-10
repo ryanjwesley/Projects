4b4b99294073f29123e17d0963ae0219bb0e1756
Ryan Wesley <ryanjwesley@gmail.com>
2020-11-10 11:25:34 -0800

    moved business object

--- Hungry/src/components/app/App.js
+++ Hungry/src/components/app/App.js
@@ -4,6 +4,18 @@ import logo from '../../logo.svg';
 import BusinessList from '../businessList/BusinessList';
 import SearchBar from '../searchBar/SearchBar';
 
+const business = {
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
 
 class App extends React.Component {
   render() {
