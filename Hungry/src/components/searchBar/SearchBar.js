5d02932755a312e962ff370e3b0342cb0b6cf9f8
Ryan Wesley <ryanjwesley@gmail.com>
2020-11-11 12:39:46 -0800

    Handle a Change in Sort Option

--- Hungry/src/components/searchBar/SearchBar.js
+++ Hungry/src/components/searchBar/SearchBar.js
@@ -1,25 +1,34 @@
 import React from 'react';
 import './SearchBar.css';
 
-const sortByOptions = {
-    'Best Match': 'best_match',
-    'Highest Rated': 'rating',
-    'Most Reviewed': 'review_count'
-};
-
 class SearchBar extends React.Component {
     constructor(props) {
         super(props);
+
         this.state = {
             term: '',
             location: '',
             sortBy: 'best_match'
         };
+        
+        this.sortByOptions = {
+            'Best Match': 'best_match',
+            'Highest Rated': 'rating',
+            'Most Reviewed': 'review_count'
+        };
+    }
+
+    getSortByClass(sortByOption) {
+        return this.state.sortBy === sortByOption ? 'active' : ''
+    }
+
+    handleSortByChange(sortByOption) {
+        this.setState({ sortBy: sortByOption});
     }
     
     renderSortByOptions() {
-        return Object.keys(sortByOptions).map(sortByOption => {
-            let sortByOptionValue = sortByOptions[sortByOption];
+        return Object.keys(this.sortByOptions).map(sortByOption => {
+            let sortByOptionValue = this.sortByOptions[sortByOption];
             return <li key={sortByOptionValue}>{sortByOption}</li>;
         });
     }
