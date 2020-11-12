efd5aa9e407697789f7a21735937083028447511
Ryan Wesley <ryanjwesley@gmail.com>
2020-11-12 14:03:14 -0800

    Add business phone number to results

--- Hungry/src/util/Yelp.js
+++ Hungry/src/util/Yelp.js
@@ -20,7 +20,8 @@ const Yelp = {
           zipCode: business.location.zip_code,
           category: business.categories[0].title,
           rating: business.rating,
-          reviewCount: business.review_count
+          reviewCount: business.review_count,
+          phoneNumber: business.display_phone
         }));
       }
     });
