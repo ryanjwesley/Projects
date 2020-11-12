ce240467dc08c83e31c459817dabd9c01e35a643
Ryan Wesley <ryanjwesley@gmail.com>
2020-11-12 12:58:49 -0800

    Modify searchYelp

--- Hungry/src/components/app/App.js
+++ Hungry/src/components/app/App.js
@@ -5,28 +5,6 @@ import BusinessList from '../businessList/BusinessList';
 import SearchBar from '../searchBar/SearchBar';
 import Yelp from '../../util/Yelp';
 
-const business = {
-  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-};
-
-const businesses = [
-business,
-business,
-business,
-business,
-business,
-business
-];
-
-
 class App extends React.Component {
   constructor(props) {
     super(props);
