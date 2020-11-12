0e12d55897475b4ec2f23b1b40c58c94022e6260
Ryan Wesley <ryanjwesley@gmail.com>
2020-11-12 13:32:52 -0800

    update check

--- Hungry/src/components/app/App.js
+++ Hungry/src/components/app/App.js
@@ -3,35 +3,35 @@ import './App.css';
 
 import BusinessList from '../businessList/BusinessList';
 import SearchBar from '../searchBar/SearchBar';
+
 import Yelp from '../../util/Yelp';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
       businesses: []
     };
-    
-    this.searchYelp = this.searchYelp.bind(this);
 
+    this.searchYelp = this.searchYelp.bind(this);
   }
-  
+
   searchYelp(term, location, sortBy) {
     Yelp.search(term, location, sortBy).then(businesses => {
-      this.setState({businesses: businesses})
+      this.setState({businesses: businesses});
     });
   }
-  
+
   render() {
     return (
       <div className="App">
-        <h1>Ravenous</h1>
+        <h1>ravenous</h1>
         <SearchBar searchYelp={this.searchYelp} />
-        <BusinessList businesses={this.state.businesses} /> 
+        <BusinessList businesses={this.state.businesses} />
       </div>
     );
   }
-};
+}
 
 export default App;
--- Hungry/src/util/Yelp.js
+++ Hungry/src/util/Yelp.js
@@ -1,31 +1,30 @@
 const apiKey = 'onkGEkqLGoBIdcWYEQ-iOld0CjDtZwZKJrlSesTA2UmF5g0cWEbXUpbvNFA82fhlixxGcJNXAEZxQvcfBgcxq5EQWLk8gVm9pvYUvj9fFu4vYQsIZFfMme4fBZWtX3Yx';
 
 const Yelp = {
-    search(term, location, sortBy) {
-        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
-                headers: {
-                    Authorization: `Bearer ${apiKey}`
-                }
-            }).then(response => {
-                return response.json();
-            }).then(jsonResponse => {
-                return (jsonResponse.business ? jsonResponse.businesses.map(business => {
-                    return {
-                        id: business.id,
-                        imageSrc: business.image_url,
-                        name: business.name,
-                        address: business.location.address1,
-                        city: business.location.city,
-                        state: business.location.state,
-                        zipCode: business.location.zip_code,
-                        category: business.categories.title,
-                        rating: business.rating,
-                        reviewCount: business.review_count
-
-                    }
-                }) : null);
-            });
-    }
-}
+  search(term, location, sortBy) {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`
+      }
+    }).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      if (jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => ({
+          id: business.id,
+          imageSrc: business.image_url,
+          name: business.name,
+          address: business.location.address1,
+          city: business.location.city,
+          state: business.location.state,
+          zipCode: business.location.zip_code,
+          category: business.categories[0].title,
+          rating: business.rating,
+          reviewCount: business.review_count
+        }));
+      }
+    });
+  }
+};
 
 export default Yelp;
