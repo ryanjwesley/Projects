b16f3dccec4d166667dd4f7720c19f0a8c4eca9f
Ryan Wesley <ryanjwesley@gmail.com>
2020-11-24 11:22:59 -0800

    create app component

---
+++ Jamming/src/components/app/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mm</span>ing</h1>
+        <div className="App">
+          {/* <SearchBar /> */}
+          <div className="App-playlist">
+            {/* <SearchResults /> */}
+            {/* <Playlist /> */}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
