b54af454f16bfec8b828b0da6d47f69274fe71ec
Ryan Wesley <ryanjwesley@gmail.com>
2020-11-24 12:23:45 -0800

    create Track component

---
+++ Jamming/src/components/Track/Track.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Track.css';
+
+class Track extends React.Component {
+    renderAction() {
+        this.props.isRemoval ? <button className="Track-action">-</button > : <button className="Track-action">+</button>;
+    };
+    
+    render() {
+        return (
+            <div className="Track">
+                <div className="Track-information">
+                    <h3>track name</h3>
+                    <p>track artist | track album </p>
+                </div>
+                {this.renderAction()}
+            </div>
+        )
+    }
+}
+
+export default Track;
