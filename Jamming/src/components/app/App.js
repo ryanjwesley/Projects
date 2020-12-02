b7b2457d69eab7b25eba2ab74a89fe08d4d9b9ce
Ryan Wesley <ryanjwesley@gmail.com>
2020-12-02 09:52:05 -0800

    Add Tracks to Ap.

--- Jamming/src/components/app/App.js
+++ Jamming/src/components/app/App.js
@@ -37,6 +37,17 @@ class App extends React.Component {
         album: 'playlistAlbum3',
         id: 6}
       ]
+    };
+    this.addTrack = this.addTrack.bind(this);
+  }
+  
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (tracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    } else {
+      tracks.push(track);
+      this.setState({ playlistTracks: tracks });
     }
   }  
   
@@ -47,7 +58,7 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} />
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
             <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
           </div>
         </div>
