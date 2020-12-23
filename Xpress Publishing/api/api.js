44549cb6844c0d2106053acdc6a5a0bc9976b45b
Ryan Wesley <ryanjwesley@gmail.com>
2020-12-23 15:06:03 -0800

    Create Series Routes

--- Xpress Publishing/api/api.js
+++ Xpress Publishing/api/api.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const artistsRouter = require('./artists');
+const seriesRouter = require('./series');
 
 const apiRouter = express.Router();
 
 apiRouter.use('/artists', artistsRouter);
+apiRouter.use('/series', seriesRouter);
 
 module.exports = apiRouter;
